92cc28cb25d0378c922ac31c5038325e30a3fa9a
Fr0xty <ongjiae8@gmail.com>
2023-01-26 13:59:01 +0800

    auto set custom status on ready

--- src/events/onlineAlert.ts
+++ src/events/onlineAlert.ts
@@ -1,5 +1,11 @@
+import { ActivityType } from 'discord.js';
 import PyonPyon from '../res/client.js';
 
 PyonPyon.on('ready', (client) => {
     console.log(`${client.user.tag} is online.`);
+
+    client.user.setActivity({
+        type: ActivityType.Watching,
+        name: 'over Pyon Pyon',
+    });
 });
